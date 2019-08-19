534548559bdacd4d93666bf0880d34c3ecd92aae
unknown <recfrag@gmail.com>
2019-08-19 19:31:30 +0300

    collision example

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 let ctx = document.getElementById('ctx').getContext('2d');
 ctx.font = '30px Arial';
 
-let x=50;
-let y=40;
-let spdX=30;
-let spdY=6;
+let x = 50;
+let spdX = 30;
+let y = 40;
+let spdY = 5;
 
-function update(){
-    x+=spdX;    
-    y+=spdY;    
-    ctx.fillRect(50, 50, x, y);
-    if(x>500){
-        console.log('boom!')
+let height = 500;
+let width = 500;
+
+function update() {
+    x += spdX;
+    y += spdY;
+    ctx.fillText('d', x, y);
+
+    if (x < 0 || x > width) {
+        spdX = -spdX
     }
-    
+    if (y < 0 || y > height) {
+        spdY = -spdY
+    }
+
 }
 
-setInterval(update, 500)
+setInterval(update, 40)
 
