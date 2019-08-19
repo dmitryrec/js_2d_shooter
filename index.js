3f44a1db5b4caeb5e87ba808c42d49a4756dc6e2
unknown <recfrag@gmail.com>
2019-08-19 18:38:04 +0300

    simple animation

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log(1)
+let ctx = document.getElementById('ctx').getContext('2d');
+ctx.font = '30px Arial';
+
+let x=50;
+let y=40;
+let spdX=30;
+let spdY=6;
+
+function update(){
+    x+=spdX;    
+    y+=spdY;    
+    ctx.fillRect(50, 50, x, y);
+    if(x>500){
+        console.log('boom!')
+    }
+    
+}
+
+setInterval(update, 500)
+
