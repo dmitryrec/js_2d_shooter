4d1b18e1885eea053201d60a0f33b633e4d000e0
unknown <recfrag@gmail.com>
2019-08-20 00:03:50 +0300

    dry && entity colliding

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ ctx.font = '30px Arial';
 
 let HEIGHT = 500;
 let WIDTH = 500;
-let message = 'Bouncing';
 
 let player = {
     x: 50,
@@ -13,22 +12,34 @@ let player = {
     name: 'P',
 };
 
-let enemy = {
-    x: 150,
-    spdX: 10,
-    y: 350,
-    spdY: 15,
-    name: 'E',
+let enemyList = {};
+
+function getDistanceBetweenEntity(entity1, entity2) {
+    var vx = entity1.x - entity2.x;
+    var vy = entity1.y - entity2.y;
+    return Math.sqrt(vx * vx + vy * vy);
 };
 
-let enemy2 = {
-    x: 150,
-    spdX: 10,
-    y: 350,
-    spdY: 15,
-    name: 'E',
+function testCollisionEntity(entity1, entity2) {
+    var distance = getDistanceBetweenEntity(entity1, entity2);
+    return distance < 30;
 };
 
+function Enemy(id, x, y, spdX, spdY) {
+    let enemy3 = {
+        x: x,
+        spdX: spdX,
+        y: y,
+        spdY: spdY,
+        name: 'E',
+        id: id,
+    };
+    enemyList[id] = enemy3;
+};
+
+Enemy('E1', 150, 150, 10, -18);
+Enemy('E2', 50, 150, 20, -8);
+Enemy('E3', 250, 150, 10, -8);
 
 function updateEntity(something) {
     something.x += something.spdX;
@@ -37,19 +48,26 @@ function updateEntity(something) {
 
 
     if (something.x < 0 || something.x > WIDTH) {
-        console.log(message);
         something.spdX = -something.spdX;
     }
     if (something.y < 0 || something.y > HEIGHT) {
-        console.log(message);
         something.spdY = -something.spdY;
     }
 };
 
-function update(){
-    updateEntity(enemy);
-    updateEntity(enemy2);
+function update() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    for (let key in enemyList) {
+        updateEntity(enemyList[key])
+
+        let isColliding = testCollisionEntity(player, enemyList[key]);
+        if(isColliding){
+            console.log('colliding!')
+        }
+    };
+
     updateEntity(player);
 }
 
-setInterval(update,40);
+setInterval(update, 40);
