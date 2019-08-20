7b458d596f0235b34ac465c8d83c569592de7ae8
unknown <recfrag@gmail.com>
2019-08-20 13:01:55 +0300

    mouse int, hp-bar, survive timer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ ctx.font = '30px Arial';
 
 let HEIGHT = 500;
 let WIDTH = 500;
+let timeWhenGameStarted = Date.now()
 
 let player = {
     x: 50,
@@ -10,11 +11,20 @@ let player = {
     y: 40,
     spdY: 5,
     name: 'P',
+    hp: 10,
 };
 
 let enemyList = {};
 
-function getDistanceBetweenEntity(entity1, entity2) {
+document.onmousemove = function (mouse) {
+    let mouseX = mouse.clientX;
+    let mouseY = mouse.clientY;
+
+    player.x = mouseX;
+    player.y = mouseY;
+}
+
+let getDistanceBetweenEntity = function (entity1, entity2) {
     var vx = entity1.x - entity2.x;
     var vy = entity1.y - entity2.y;
     return Math.sqrt(vx * vx + vy * vy);
@@ -25,7 +35,7 @@ function testCollisionEntity(entity1, entity2) {
     return distance < 30;
 };
 
-function Enemy(id, x, y, spdX, spdY) {
+let Enemy = function (id, x, y, spdX, spdY) {
     let enemy3 = {
         x: x,
         spdX: spdX,
@@ -37,15 +47,13 @@ function Enemy(id, x, y, spdX, spdY) {
     enemyList[id] = enemy3;
 };
 
-Enemy('E1', 150, 150, 10, -18);
-Enemy('E2', 50, 150, 20, -8);
-Enemy('E3', 250, 150, 10, -8);
+let drawEntity = function (something) {
+    ctx.fillText(something.name, something.x, something.y);
+};
 
-function updateEntity(something) {
+let updateEntityPosition = function (something) {
     something.x += something.spdX;
     something.y += something.spdY;
-    ctx.fillText(something.name, something.x, something.y);
-
 
     if (something.x < 0 || something.x > WIDTH) {
         something.spdX = -something.spdX;
@@ -55,19 +63,36 @@ function updateEntity(something) {
     }
 };
 
-function update() {
+let updateEntity = function (something) {
+    updateEntityPosition(something);
+    drawEntity(something);
+};
+
+let update = function () {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
     for (let key in enemyList) {
         updateEntity(enemyList[key])
 
         let isColliding = testCollisionEntity(player, enemyList[key]);
-        if(isColliding){
-            console.log('colliding!')
+        if (isColliding) {
+            player.hp -= 1;
+            if (player.hp <= 0) {
+                let timeSurvived = Date.now() -timeWhenGameStarted;
+                console.log('you lost! time survived is ' + timeSurvived/1000 + ' sec');
+                timeWhenGameStarted= Date.now();
+                player.hp = 10;
+            }
         }
     };
 
-    updateEntity(player);
-}
+    drawEntity(player);
+    ctx.fillText(player.hp + ' hp', 0, 25);
+};
+
+
+Enemy('E1', 150, 150, 10, -18);
+Enemy('E2', 50, 150, 20, -8);
+Enemy('E3', 250, 150, 10, -8);
 
 setInterval(update, 40);
